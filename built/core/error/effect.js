b5f97abb1b44c2d7983988e935c0540962ee29b6
Владислав <85783103+Destrokhen-main@users.noreply.github.com>
2022-11-17 11:51:44 +0300

    Delete built directory

--- built/core/error/effect.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var message = {
-    NEED_FINCTION: "Первым параметром должна идти функция",
-    DEP_NEED_ARRAY: "Зависимости могут быть только в массиве",
-    ONLY_PROXY: "Вы попытались использовать в зависимости не proxy"
-};
-exports.default = message;
-//# sourceMappingURL=effect.js.map
--- built/core/error/watch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var message = {
-    NEED_DEP: "Нет зависимостей для",
-    NEED_PROXY: "Вы можете наблюдать только за proxy"
-};
-exports.default = message;
-//# sourceMappingURL=watch.js.map
