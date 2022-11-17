0eca6880b6705763175e0c78a9dfa99c42cbbd03
Vlad <armagiddon19@gmail.com>
2022-11-17 11:42:50 +0300

    added more test

--- test/i.test.ts
+++ test/i.test.ts
@@ -8,9 +8,20 @@ const comp = function() {
   }
 } as any
 
+const comp1 : any = () => {
+  return {
+    tag: "div",
+    child: "hello"
+  }
+}
+
 describe('Hello Component', () => {
-  test('build component', () => {
+  test('build component test-1', () => {
     const s = builder(comp);
     expect(s).toStrictEqual({ tag: 'div', child: [ { type: 0, value: 'hello' } ], type: 2 });
   });
+  test('build component test-2', () => {
+    const s = builder(comp1);
+    expect(s).toStrictEqual({ tag: 'div', child: [ { type: 0, value: 'hello' } ], type: 2 });
+  })
 });
