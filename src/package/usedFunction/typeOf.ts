0b4ff4ac8ff9981fb3025c14866b01bea6ed2b0f
Vlad <armagiddon19@gmail.com>
2023-01-20 10:24:04 +0300

    init

--- src/package/usedFunction/typeOf.ts
+++ src/package/usedFunction/typeOf.ts
@@ -1,12 +1,20 @@
+// TODO [ ] - isProxy with null 
+
 import { ProxyType } from "../reactive/type";
 
 const isProxy = function (obj: any) {
+  if (typeof obj === null || typeof obj === undefined) return false;
   return obj.type === ProxyType.Proxy ? true : false;
 };
 
 export const typeOf = function (obj: any) {
-  const type = typeof obj;
+  const type : string = typeof obj;
+  console.log(type);
+  if (type === "null") return "null";
+  if (type === "undefined") return "undefined";
+
   if (type === "object") {
+    console.log(type);
     if (isProxy(obj)) {
       return ProxyType.Proxy;
     } else if (Array.isArray(obj)) {
