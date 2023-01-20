5d0a109f5cef0d8056a37e3c8c29c4310b77e1cf
Vlad <armagiddon19@gmail.com>
2023-01-20 10:27:37 +0300

    undefined and null

--- src/package/usedFunction/typeOf.ts
+++ src/package/usedFunction/typeOf.ts
@@ -3,18 +3,15 @@
 import { ProxyType } from "../reactive/type";
 
 const isProxy = function (obj: any) {
-  if (typeof obj === null || typeof obj === undefined) return false;
   return obj.type === ProxyType.Proxy ? true : false;
 };
 
 export const typeOf = function (obj: any) {
   const type : string = typeof obj;
-  console.log(type);
-  if (type === "null") return "null";
-  if (type === "undefined") return "undefined";
+  if (obj === null) return "null";
+  if (obj === undefined) return "undefined";
 
   if (type === "object") {
-    console.log(type);
     if (isProxy(obj)) {
       return ProxyType.Proxy;
     } else if (Array.isArray(obj)) {
