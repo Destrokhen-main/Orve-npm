3193dd67dc6d2f8d12effedaafb23136b3bea944
Destrokhen <armagiddon19@gmail.com>
2023-05-16 21:44:57 +0300

    added new test in component

--- test/component.test.ts
+++ test/component.test.ts
@@ -1,5 +1,6 @@
 import {describe, expect, test} from '@jest/globals';
 import { parser } from "../src/package/dom/builder/index";
+import orve from "../src/index";
 
 const comp = function() {
   return {
@@ -36,4 +37,84 @@ describe('Hello Component without mounted', () => {
     const object = parser(comp1);
     t(object);
   });
+});
+
+
+// Check parse app with Layer 
+const Component1 = function() {
+  return {
+    tag: Layer,
+    child: ["Hello"]
+  }
+}
+
+const Layer = function({ children }: { children: any }) {
+  return {
+    tag: "div",
+    child: [
+      children,
+      "World"
+    ]
+  }
+}
+// ------
+
+
+// check parse app with Layer and Props
+
+function Layer1({ children, classes } : { children: any, classes: any }) {
+  return {
+    tag: "div",
+    props: {
+      class: classes
+    },
+    child: [
+      children[0] + " 1"
+    ]
+  }
+}
+
+function Component2() {
+  return {
+    tag: Layer1,
+    props: {
+      id: 123,
+      classes: "test"
+    },
+    child: "test"
+  }
+}
+
+// -----
+
+
+describe('Component with layer', () => {
+  test("Check parse app with Layer", () => {
+    const app = parser(Component1);
+
+    expect(Array.isArray(app?.child)).toBe(true);
+
+    expect(app?.child.length).toBe(2);
+
+    app?.child.forEach((e: any) => {
+      expect(e.type).toBe("Static");
+    });
+
+    expect(app?.child[0].value).toBe("Hello");
+    expect(app?.child[1].value).toBe("World");
+  })
+
+  test("check parse app with Layer and Props", () => {
+    const app = parser(Component2);
+
+    expect(app?.props).toBeDefined();
+    expect(app?.props?.class).toBeDefined();
+    expect(app?.props?.class).toBe("test");
+
+    expect(app?.child).toBeDefined();
+
+    expect(app?.child.length).toBe(1);
+
+    expect(app?.child[0].value).toBe("test 1");
+  })
 });
