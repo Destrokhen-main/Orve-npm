c5661acc588a2cf9f0bfd0cb9308ab3158058e0d
Destrokhen <armagiddon19@gmail.com>
2023-05-24 22:50:33 +0300

    fix index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,36 @@
-export { createApp } from "./package/dom/index";
-export { refL } from "./package/reactive/refL";
-export { ref } from "./package/reactive/ref";
-export { refC } from "./package/reactive/refC";
-export { watch } from "./package/reactive/watch";
-export { refO } from "./package/reactive/refO";
-export { refA } from "./package/reactive/refA";
-export { effect } from "./package/reactive/effect";
-export { oif } from "./package/reactive/oif"; 
-export { Orve, context } from "./package/default";
+import { createApp } from "./package/dom/index";
+import { refL } from "./package/reactive/refL";
+import { ref } from "./package/reactive/ref";
+import { refC } from "./package/reactive/refC";
+import { watch } from "./package/reactive/watch";
+import { refO } from "./package/reactive/refO";
+import { refA } from "./package/reactive/refA";
+import { effect } from "./package/reactive/effect";
+import { oif } from "./package/reactive/oif"; 
+import { Orve, context } from "./package/default";
 import { Node, Fragment } from "./package/jsx";
-export { Node, Fragment } from "./package/jsx";
-export * from "./package/dom/root";
-// export {
-//   createApp,
-//   refL,
-//   ref,
-//   refC,
-//   watch,
-//   refO,
-//   refA,
-//   effect,
-//   Orve,
-//   Node,
-//   Fragment,
-//   oif,
-//   context
-// };
-
-export default {
+import { createAppRoot } from "./package/dom/root";
+export {
+  createApp,
+  refL,
+  ref,
+  refC,
+  watch,
+  refO,
+  refA,
+  effect,
+  Orve,
   Node,
   Fragment,
+  oif,
+  context,
+  createAppRoot
+};
+
+export default {
+  createApp,
+  ...Orve,
+  Node,
+  Fragment,
+  createAppRoot
 };
