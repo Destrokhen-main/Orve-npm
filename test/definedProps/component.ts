2fe0c7edc2e093af40531090f924b17964e4b1d9
Пысларь Владислав <vypyslar@mts.ru>
2023-06-10 15:59:53 +0300

    added some tests

--- test/definedProps/component.ts
+++ test/definedProps/component.ts
@@ -2,13 +2,52 @@ import { Node } from "../../src/package/jsx"
 import { defineProps } from "../../src";
 
 function comp() {
-  return Node("div");
+  return Node("div", {});
 }
 
-export const Component = defineProps(comp, {
+/**
+ * R - required true
+ * D - set default
+ */
+export const cOneStringD = defineProps(comp, {
   st: {
     type: String,
     required: false,
     default: "asd"
+  }
+})
+
+export const cOneStringRD = defineProps(comp, {
+  st: {
+    type: String,
+    required: true,
+    default: "asd"
+  }
+})
+
+export const cOneString = defineProps(comp, {
+  st: {
+    type: String
+  }
+})
+
+export const cOneNumber = defineProps(comp, {
+  st: {
+    type: Number,
+  }
+})
+
+export const cOneNumberR = defineProps(comp, {
+  st: {
+    type: Number,
+    required: true
+  }
+})
+
+export const cOneNumberD = defineProps(comp, {
+  st: {
+    type: Number,
+    required: false,
+    default: 123
   }
 })
--- test/definedProps/definedProps.test.ts
+++ test/definedProps/definedProps.test.ts
@@ -1,9 +1,35 @@
 import { expect, test, describe } from "@jest/globals";
 
-import { Component } from "./component";
+import { cOneStringD, cOneStringRD, cOneString, cOneNumber, cOneNumberD } from "./component";
 
 describe("definedProps", () => {
-  test("props check", () => {
-    const newCom
+  test("props String D not set", () => {
+    const comp = cOneStringD({});
+    expect(comp?.props?.st).toBe("asd")
+  })
+
+  test("props String RD", () => {
+    const comp = cOneStringRD({});
+    expect(comp?.props).not.toBeDefined()
+  })
+
+  test("props String", () => {
+    const comp = cOneString({});
+    expect(comp?.props).not.toBeDefined()
+  })
+
+  test("props String RD have props", () => {
+    const comp = cOneStringRD({ st: "test" });
+    expect(comp?.props?.st).toBe("test");
+  })
+
+  test("props Number", () => {
+    const comp = cOneNumber({});
+    expect(comp?.props).not.toBeDefined()
+  })
+
+  test("props Number D", () => {
+    const comp = cOneNumberD({});
+    expect(comp?.props?.st).toBe(123);
   })
 })
